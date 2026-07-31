1ed89340c22a304d34426f3f9d9c96e5f0e03ca3
Promise <promiseihunna@gmail.com>
2026-07-31 19:17:16 +0100

    Refactor database connection logic in postgres.go to improve readability by removing unnecessary whitespace; update build-errors.log to consolidate exit status messages.

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -20,22 +20,18 @@ func Connect(databaseURL string) (*pgxpool.Pool, error) {
 	}
 
 	var pool *pgxpool.Pool
-
 	pool, err = pgxpool.NewWithConfig(ctx, config)
-
 	if err != nil {
 		slog.Error("Unable to create connection pool", "message", err.Error())
 		return nil, err
 	}
 
 	err = pool.Ping(ctx)
-
 	if err != nil {
 		slog.Error("Unable to ping database", "message", err.Error())
 		return nil, err
 	}
 
 	slog.Info("Successfully connected to the database!")
-
 	return pool, nil
 }
