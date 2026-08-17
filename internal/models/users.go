9c9e6ba93310ab1dd03b3859ff437a9ff2425a2c
Promise <promiseihunna@gmail.com>
2026-08-17 02:44:51 +0100

    Update Users model to hide password field in JSON responses for enhanced security.

--- internal/models/users.go
+++ internal/models/users.go
@@ -5,7 +5,7 @@ import "time"
 type Users struct {
 	ID        string    `json:"id" db:"id"`
 	Email     string    `json:"email" db:"email"`
-	Password  string    `json:"password" db:"password"`
+	Password  string    `json:"-" db:"password"`
 	Username  string    `json:"username" db:"username"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
